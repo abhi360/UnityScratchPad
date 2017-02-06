23a1a0fd6ee0ad27bd8b5ea057b8e3e025e0f6ad
abhi360 <manyu360@gmail.com>
2017-02-06 16:30:00 +0530

    ObjectPooling
    - object pooling basics

---
+++ Assets/Scripts/Spawnable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnable : PooledObject
+{
+    public Rigidbody Body { get; private set; }
+
+    void Awake()
+    {
+        Body = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider enteredCollider)
+    {
+        if (enteredCollider.CompareTag("KillZone"))
+        {
+            ReturnToPool();
+        }
+    }
+
+    public void SetMaterial(Material mat)
+    {
+        GetComponent<Renderer>().material = mat;
+        for (int i = 0; i < transform.childCount; ++i)
+            transform.GetChild(i).GetComponent<Renderer>().material = mat;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public FloatRange timeBetweenSpawns, scale, randomVelocity, angularVelocity;
+
+    public Spawnable[] spawnablePrefab;
+
+    public Material[] materials;
+
+    public float velocity;
+
+    private float timeSinceLastSpawn;
+    private float currentSpawnDelay;
+
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= currentSpawnDelay)
+        {
+            timeSinceLastSpawn -= currentSpawnDelay;
+            currentSpawnDelay = timeBetweenSpawns.RandomInRange;
+            SpawnStuff();
+        }
+    }
+
+    void SpawnStuff()
+    {
+        Spawnable prefab = spawnablePrefab[Random.Range(0, spawnablePrefab.Length)];
+        Spawnable spawn = prefab.GetPooledInstance<Spawnable>();
+
+        spawn.transform.localPosition = transform.position;
+        spawn.transform.localScale = Vector3.one * scale.RandomInRange;
+        spawn.transform.localRotation = Random.rotation;
+
+        spawn.Body.velocity = transform.up * velocity +
+            Random.onUnitSphere * randomVelocity.RandomInRange;
+
+        spawn.Body.angularVelocity =
+            Random.onUnitSphere * angularVelocity.RandomInRange;
+
+        spawn.SetMaterial(materials[Random.Range(0, materials.Length)]);
+    }
+}
+
+
+[System.Serializable]
+public struct FloatRange
+{
+
+    public float min, max;
+
+    public float RandomInRange
+    {
+        get
+        {
+            return Random.Range(min, max);
+        }
+    }
+}
