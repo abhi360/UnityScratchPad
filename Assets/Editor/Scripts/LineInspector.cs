9dd02ad17520eab6a910e68ca0fb68e32bfc55c5
abhi360 <manyu360@gmail.com>
2017-03-20 17:02:21 +0530

    splines and curves added
    - added line
    - added bezier curve
    - added splines

---
+++ Assets/Editor/Scripts/LineInspector.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+    private const float handleSize = 0.04f;
+    private const float pickSize = 0.06f;
+
+    private int selectedIndex = 0;
+
+    private void OnSceneGUI()
+    {
+        Line line = target as Line;
+        Transform handleTransform = line.transform;
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+
+        Handles.color = Color.white;
+        Handles.DrawLine(p0, p1);
+
+        float size = HandleUtility.GetHandleSize(p0);
+        if (Handles.Button(p0, handleRotation, size * handleSize, size * pickSize, Handles.DotCap))
+        {
+            selectedIndex = 0;
+            Repaint();
+        }
+        if (Handles.Button(p1, handleRotation, size * handleSize, size * pickSize, Handles.DotCap))
+        {
+            selectedIndex = 1;
+            Repaint();
+        }
+
+        if (selectedIndex == 0)
+        {
+            EditorGUI.BeginChangeCheck();
+            p0 = Handles.DoPositionHandle(p0, handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(line, "Move Point");
+                EditorUtility.SetDirty(line);
+                line.p0 = handleTransform.InverseTransformPoint(p0);
+            }
+        }
+
+        if (selectedIndex == 1)
+        {
+            EditorGUI.BeginChangeCheck();
+            p1 = Handles.DoPositionHandle(p1, handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(line, "Move Point");
+                EditorUtility.SetDirty(line);
+                line.p1 = handleTransform.InverseTransformPoint(p1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SplineWalker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineWalker : MonoBehaviour
+{
+    public bool lookForward;
+    public BezierSpline spline;
+
+    public float duration;
+
+    private float progress;
+
+    public float speed;
+
+    private void Update()
+    {
+        progress += Time.deltaTime / duration * speed;
+        if (progress > 1f)
+        {
+            progress = 1f;
+        }
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+        if (lookForward)
+        {
+            transform.LookAt(position + spline.GetDirection(progress));
+        }
+    }
+}
